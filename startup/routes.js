997a1666c7808327b43849fb563e1449fe6a6190
ThomasSKokholm <ThomasSKokholm@gmail.com>
2022-04-01 09:50:00 +0200

    added root site

--- startup/routes.js
+++ startup/routes.js
@@ -6,10 +6,12 @@ const product = require('../routes/product');
 const farmer = require('../routes/farmer');
 const user = require('../routes/user');
 
+
 module.exports = function (app) {
     app.use(helmet());
     app.use(compression());
     app.use(cors());
+    app.get("/", (req,res)=>{res.send("Det virker!")});
     app.use("/api/products", products);
     app.use("/api/product", product);
     app.use("/api/farmer", farmer);
